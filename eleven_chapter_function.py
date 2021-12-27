1b21be18581e968a53ec36e313e5adacae407479
ikhojiakbar <95846758+ikhojiakbar@users.noreply.github.com>
2021-12-28 06:43:08 +0900

    Add files via upload

---
+++ eleven_chapter_function.py
@@ -0,0 +1,8 @@
+def get_formatted_name(first_name, last_name, middle_name=''):
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+
+    return full_name.title()
+
